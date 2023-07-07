f0e52e7c39f6492bd8c28b64dba23c9632567d1e
Yancenteno <yancie.centeno@gmail.com>
2023-07-06 21:05:12 -0400

    Commit

--- Algos/Week1/day3.js
+++ Algos/Week1/day3.js
@@ -58,3 +58,50 @@ console.log(selectionSort(numbersRandomOrder));
 //Temp variable to swap
 
 /*****************************************************************************/
+
+// const numbersOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const numbersRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+// const numbersReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+// const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * Sorts given array in-place.
+ * Best: O(n^2) quadratic.
+ * Average: O(n^2) quadratic.
+ * Worst: O(n^2) quadratic.
+ * @param {Array<number>} numbers
+ * @returns {Array<number>} The given array after being sorted.
+ */
+function selectionSort(numbers = []) {
+
+    for (let i=0;i<numbers.length;i++){
+        const selectedNumber = numbers[i]
+
+        let indexOfMin = i
+        let currentMin = numbers[indexOfMin]
+
+        for (let j=i+1; j<numbers.length; j++){
+
+            let jNumber = numbers[j]
+
+            if (jNumber < currentMin){
+                indexOfMin = j
+                currentMin = numbers[j]
+            }
+
+        }
+
+        if (currentMin < selectedNumber) {
+            let temp = selectedNumber
+
+            numbers[i] = currentMin
+            numbers[indexOfMin] = temp
+        }
+        
+    }
+
+    return numbers
+
+}
+
+console.log(selectionSort(numbersRandomOrder))
--- React/Lifting_State/lifting_state/src/components/UserForm.jsx
+++ React/Lifting_State/lifting_state/src/components/UserForm.jsx
@@ -1,59 +1,133 @@
 import React, { useState } from 'react';
+import './myStyle.css'
 
 const UserForm = (props) => {
 
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    })
+
+    const [firstErr, setFirstErr] = useState("")
+    const [lastErr, setLastErr] = useState("")
+    const [mailErr, setMailErr] = useState("")
+    const [passErr, setPassErr] = useState("")
+    const [conPassErr, setConPassErr] = useState("")
+
+
 
     const createUser = (e) => {
         e.preventDefault();
 
-        const newUser = { firstName, lastName, email, password, confirmPassword };
-        setFirstName("");
-        setLastName("");
-        setEmail("");
-        setPassword("");
-        setConfirmPassword("");
+
+        console.log(formData)
+
+        setFormData({
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            confirmPassword: ""
+        })
     };
 
+    const handleChange = e => {
+        const { name, value } = e.target
+        setFormData((currentData) => ({ ...currentData, [name]: value }))
+        // setFormData({ ...formData, [name]: value })
+
+        if (name === "firstName" && value.length < 2) {
+            setFirstErr("First Name must be at least 2 characters!")
+        } else {
+            setFirstErr("")
+        }
+
+        if (name === "lastName" && value.length < 2) {
+            setLastErr("Last Name must be at least 2 characters!")
+        } else {
+            setLastErr("")
+        }
+
+        if (name === "email" && value.length < 2) {
+            setMailErr("Email must be at least 2 characters!")
+        } else {
+            setMailErr("")
+        }
+
+        if (name === "password" && value.length < 8) {
+            setPassErr("Password must be at least 8 characters!")
+        } else {
+            setPassErr("")
+        }
+
+        if (name === "confirmPassword" && value !== formData.password) {
+            setConPassErr("Passwords Must match!")
+        } else {
+            setConPassErr("")
+        }
+
+    }
+
+    const errStyle = {
+        margin: 0,
+        padding: 0,
+        color: "red",
+        fontWeight: "bold",
+        textAlign: "left"
+    }
+
+
     return (
-        <div>
+        <div className='main'>
             <form onSubmit={createUser}>
-                <div>
-                    <label>First Name:</label>
-                    <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-                </div>
-                <div>
-                    <label>Last Name:</label>
-                    <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-                </div>
-                <div>
-                    <label>Email:</label>
-                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                </div>
-                <div>
-                    <label>Confirm Password:</label>
-                    <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-                </div>
-                <input type="submit" value="Create User" />
+                <fieldset>
+                    <legend>Register</legend>
+                    <div className='box'>
+                        <label>First Name:</label>
+                        <input type="text" name='firstName' onChange={handleChange} value={formData.firstName} />
+                    </div>
+
+                    {formData.firstName && <p style={errStyle} >{firstErr}</p>}
+
+                    <div className='box'>
+                        <label>Last Name:</label>
+                        <input type="text" name='lastName' onChange={handleChange} value={formData.lastName} />
+                    </div>
+
+                    {formData.lastName && <p style={errStyle} >{lastErr}</p>}
+
+                    <div className='box'>
+                        <label>Email:</label>
+                        <input type="text" name='email' onChange={handleChange} value={formData.email} />
+                    </div>
+
+                    {formData.email && <p style={errStyle} >{mailErr}</p>}
+
+                    <div className='box'>
+                        <label>Password:</label>
+                        <input type="password" name='password' onChange={handleChange} value={formData.password} />
+                    </div>
+
+                    {formData.password && <p style={errStyle} >{passErr}</p>}
+
+                    <div className='box'>
+
+                        <label>Confirm Password:</label>
+                        <input type="password" name='confirmPassword' onChange={handleChange} value={formData.confirmPassword} />
+                    </div>
+
+                    {formData.confirmPassword && <p style={errStyle} >{conPassErr}</p>}
+
+                    <input type="submit" value="Create User" />
+                </fieldset>
             </form>
-            <div>
-                <h2>Your Form Data</h2>
-                <p>First Name: {firstName}</p>
-                <p>Last Name: {lastName}</p>
-                <p>Email: {email}</p>
-                <p>Password: {password}</p>
-                <p>Confirm Password: {confirmPassword}</p>
-            </div>
         </div>
     )
 }
 
 export default UserForm
+
+
