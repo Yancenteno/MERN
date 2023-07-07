50280502cdee9dd10515b0742947972161828233
Yancenteno <yancie.centeno@gmail.com>
2023-07-07 12:23:27 -0400

    Commit

--- React/Lifting_State/lifting_state/src/App.jsx
+++ React/Lifting_State/lifting_state/src/App.jsx
@@ -1,13 +1,16 @@
-import UserForm from './components/UserForm';
+// import UserForm from './components/UserForm';
+import BoxGenerator from './components/BoxGenerator';
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <UserForm />
+      {/* <UserForm /> */}
+      <BoxGenerator />
     </div>
   );
 }
+
 
 export default App;
---
+++ React/Lifting_State/lifting_state/src/components/BoxGenerator.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function BoxGenerator() {
+  return (
+    <div>
+        <form action="">
+            <label>Color</label>
+            <input type="color" />
+        </form>
+    </div>
+  )
+}
+
+export default BoxGenerator
